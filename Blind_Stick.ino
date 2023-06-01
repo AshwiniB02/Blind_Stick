b713cd71541f5fe54636f6e4f7f47d10c3a4ef6c
Ashwini B <ashwini02abp02@gmail.com>
2023-06-01 21:13:41 +0530

    both ultrasonic working properly

--- Blind_Stick.ino
+++ Blind_Stick.ino
@@ -1,23 +1,60 @@
-// #include <Ultrasonic.h>
-
-// OOP => Object Oriented Programming
-// class object => Ultrasonic
-// instances => make two instances
-// Ultrasonic ultrasonic_top(4, 5);
-// Ultrasonic ultrasonic_bottom(6, 7);
+const int top_trig = 2;      // D4
+const int top_echo = 14;     // D5
+const int bottom_trig = 12;  // D6
+const int bottom_echo = 13;  // D7
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(top_trig, OUTPUT);
+  pinMode(top_echo, INPUT);
+  pinMode(bottom_trig, OUTPUT);
+  pinMode(bottom_echo, INPUT);
+}
+
+float readTopSensor() {
+  digitalWrite(top_trig, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(top_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(top_trig, LOW);
+
+  long duration = pulseIn(top_echo, HIGH);
+  float distance = duration / 58.2;
+
+  /*Serial.print("top distance: ");
+  Serial.print(distance);
+  Serial.println("cm");*/
+
+  return distance;
+}
+
+float readBottomSensor() {
+  digitalWrite(bottom_trig, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(bottom_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(bottom_trig, LOW);
+
+  long duration = pulseIn(bottom_echo, HIGH);
+  float distance = duration / 58.2;
+
+  /*Serial.print("distance bottom: ");
+  Serial.print(distance);
+  Serial.println("cm");*/
+
+  return distance;
 }
 
 void loop() {
-  Serial.print("Sensor top: ");
-  // Serial.print(ultrasonic_top.read()); // Prints the distance on the default unit (centimeters)
-  Serial.println("cm");
-
-  Serial.print("Sensor bottom: ");
-  // Serial.print(ultrasonic_bottom.read(CM)); // Prints the distance making the unit explicit
-  Serial.println("cm");
-
+  float top=readTopSensor();
   delay(200);
+  float bottom=readBottomSensor();
+  delay(200);
+  Serial.print("Distance: top ");
+  Serial.print(top);
+  Serial.print(" Distance : bottom ");
+  Serial.println(bottom);
 }
